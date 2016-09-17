c4383cbb074861098d25203e672c5919ac6eb4d7
daniestrijdom <daansteraan@gmail.com>
2016-09-17 21:27:12 +0200

    using a decision tree on the same dataset
    ... significantly worse

---
+++ TESTS/wine_dtree.py
@@ -0,0 +1,39 @@
+'''
+decision tree on red wine quality
+'''
+# TODO: get data
+import pandas as pd
+
+df = pd.read_csv("winequality-red.csv", delimiter=";")
+
+# TODO: Specify testing and trainig sets
+
+train_idx = [i for i in range(1550,1599)]
+train = df.drop(train_idx, axis=0)
+
+
+test_idx = [ i for i in range(0,1550)]
+test = df.drop(test_idx, axis=0)
+test = test.reset_index(drop=True)
+
+# TODO: Fit decision tree model
+# from sklearn import tree
+from sklearn import linear_model
+
+# clf = tree.DecisionTreeClassifier()
+clf = linear_model.LinearRegression()
+
+clf.fit(train.drop("quality",axis=1),train.quality)
+
+# print train.head()
+
+predict = list(pd.to_numeric(pd.Series(clf.predict(test.drop('quality',axis=1)))))
+e = 0
+for i in range(len(predict)):
+    predict[i] = int(round(predict[i],0))
+
+actual = list(test.quality)
+
+from sklearn.metrics import accuracy_score
+
+print accuracy_score(actual, predict)
